3176150e4c03fff62a6321faccc360d43eafbe8c
LSanasi <LSanasi@users.noreply.github.com>
2023-11-01 14:11:37 +0100

    MousePosition script modified
    MousePosition script modified to display EPSG: 3003

--- js/L.Control.MousePosition.js
+++ js/L.Control.MousePosition.js
@@ -23,13 +23,18 @@ L.Control.MousePosition = L.Control.extend({
   },
 
   _onMouseMove: function (e) {
-    var lng = this.options.lngFormatter ? this.options.lngFormatter(e.latlng.lng) : L.Util.formatNum(e.latlng.lng, this.options.numDigits);
-    var lat = this.options.latFormatter ? this.options.latFormatter(e.latlng.lat) : L.Util.formatNum(e.latlng.lat, this.options.numDigits);
+    var wgs84LatLng = e.latlng; // Latitude and longitude in WGS84 (EPSG:4326)
+    
+    // Perform coordinate transformation from WGS84 to EPSG:3003 using Proj4js
+    var epsg3003Coordinates = proj4('EPSG:3003', [wgs84LatLng.lng, wgs84LatLng.lat]);
+    
+    var lng = this.options.lngFormatter ? this.options.lngFormatter(epsg3003Coordinates[0]) : L.Util.formatNum(epsg3003Coordinates[0], this.options.numDigits);
+    var lat = this.options.latFormatter ? this.options.latFormatter(epsg3003Coordinates[1]) : L.Util.formatNum(epsg3003Coordinates[1], this.options.numDigits);
+    
     var value = this.options.lngFirst ? lng + this.options.separator + lat : lat + this.options.separator + lng;
     var prefixAndValue = this.options.prefix + ' ' + value;
     this._container.innerHTML = prefixAndValue;
   }
-
 });
 
 L.Map.mergeOptions({
